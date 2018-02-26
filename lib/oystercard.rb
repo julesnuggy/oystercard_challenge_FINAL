213cdfffaa843ee5ad1f550b8b5b243abbc1093b
durranee <durranee@hotmail.com>
2018-02-26 15:13:14 +0000

    created initilaized and default balance test passed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,2 +1,8 @@
 class OysterCard
+  attr_accessor :balance
+  DEFAULT_BAL = 0
+
+  def initialize(balance = DEFAULT_BAL)
+    @balance = balance
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,10 @@
 require 'oystercard'
 
 describe OysterCard do
+  subject(:oystercard) { OysterCard.new(0) } # Default balance = 0
+   # Creating a new OysterCard above bal = 0
 
+  it 'should have default balance of 0' do
+    expect(oystercard.balance).to eq(0)
+  end
 end
