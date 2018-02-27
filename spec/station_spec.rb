04e3b6b13259687f82e0b0a224eb662c5269bf35
durranee <durranee@hotmail.com>
2018-02-27 16:42:24 +0000

    Station Class and tests completed

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,7 +1,13 @@
 require 'station'
 
 describe Station do
-subject(:station) { OysterCard.new("Barking", 4) }
+subject(:station) { Station.new("Canning Town", 2) }
 
+it 'knows its name' do
+   expect(station.name).to eq("Canning Town")
+ end
 
+ it 'knows its zone' do
+   expect(station.zone).to eq(2)
+ end
 end
